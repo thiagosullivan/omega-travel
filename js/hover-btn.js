dcffb1ae1cccf14872b5585af0c2b3d85067b72c
Thiago Sullivan <thiago.sullivan.dev@gmail.com>
2019-04-24 13:22:52 -0300

    first commit correcting errors

---
+++ js/hover-btn.js
@@ -0,0 +1,22 @@
+// var btnImg = document.getElementById('img-cont3');
+
+// btnImg = document.addEventListener("mouseover", function() {
+//     var x = document.getElementById("btn-js");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   });
+// btnImg = document.addEventListener("mouseout", function(){
+//     console.log("aqui 2");
+// })
+
+function showSix (){
+    var element = document.getElementById('btn-js');
+    if (element.style.display == 'none'){
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.slider-reviews').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1
+});
